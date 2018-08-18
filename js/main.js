8d3833a91c486450f96a5e2a0c038d983202f136
Olusola David <sola.dav7@gmail.com>
2018-08-18 03:11:58 +0100

    feat(Entries): fetch user entries on successful authentication
    - consume GET /entries
    
    [Delivers #159484907]

--- js/main.js
+++ js/main.js
@@ -1,5 +1,73 @@
-// ---------------------- Globals -----------------------
+// ---------------------- Globals and Utilities-----------------------
 const baseUrl = 'https://my-diary-api.herokuapp.com/api/v1';
+
+/**
+ * Parses query string into JSON object
+ * - string format is the format returned by location.search
+ * @param {*} queryString
+ */
+const queryToJSON = (queryString) => {
+  if (!(queryString.includes('?') && queryString.includes('='))) return {};
+  const queryStr = queryString.slice(1).split('&');
+  const queryJSON = {};
+  queryStr.forEach((keyValue) => {
+    const keyValArr = keyValue.split('=');
+    queryJSON[keyValArr[0]] = decodeURIComponent(keyValArr[1] || '');
+  });
+  return queryJSON;
+};
+
+const jsonToQuery = (queryObj) => {
+  if (!Object.keys(queryObj).length) return '';
+  const queryKeys = Object.keys(queryObj);
+  const queryKeyValue = queryKeys.map(key => `${key}=${encodeURIComponent(queryObj[key])}`);
+  const queryString = `?${queryKeyValue.join('&')}`;
+  return queryString;
+};
+
+const handleCommonErrors = (status, resp) => {
+  switch (status) {
+    case 401:
+      window.location.replace('login.html');
+      break;
+    default:
+      // TODO: toast the error message
+  }
+};
+
+// const makeToast = (type, message) => {
+// }
+
+const reportNetworkError = () => {
+  // toast message here
+};
+
+const makeRequest = async (method, requestUrl, body = null,
+  successCb, catchCb = reportNetworkError, finallyCb) => {
+  // prepare request
+  const token = localStorage.getItem('accessToken');
+  const request = {
+    method,
+    mode: 'cors',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  // send request
+  try {
+    const rawResponse = await fetch(requestUrl, request);
+    const { status } = rawResponse;
+    const parsedResponse = await rawResponse.json();
+    successCb(status, parsedResponse);
+  } catch (err) {
+    catchCb();
+  } finally {
+    finallyCb();
+  }
+};
+
 
 // ---------------------- Responsive side menu --------------------
 
@@ -23,19 +91,15 @@ const toggleSideBar = () => {
 };
 
 if (closeMenuButton) closeMenuButton.addEventListener('click', hideSideBar);
-if (openMenuButton) openMenuButton.addEventListener('focus', showSideBar);
+if (openMenuButton) openMenuButton.addEventListener('click', showSideBar);
 window.addEventListener('resize', toggleSideBar);
 
-// -------------------- Authentication functions --------------
+// -------------------- Authentication and form functions --------------
 
 const signupForm = document.querySelector('.js-form-signup');
-
 const loginForm = document.querySelector('.js-form-login');
-
 const logoutBtn = document.querySelector('.js-logout');
-
 const error = document.querySelector('.js-error-field');
-
 const formFields = document.querySelectorAll('.form__input');
 
 const showError = (msg) => {
@@ -103,9 +167,9 @@ const processSignup = async (e) => {
     if (status === 201) {
       localStorage.setItem('accessToken', parsedResponse.token);
       window.location.replace('stories.html');
-    } else if (Array.isArray(parsedResponse.error.message)) {
-      const errorList = parsedResponse.error.message;
-      let message = errorList.reduce((errStr, errObj) => `${errStr}<br>* ${errObj.msg}`, '');
+    } else if (status === 400) {
+      const errorList = parsedResponse.error;
+      let message = errorList.reduce((errStr, errObj) => `${errStr}* ${errObj.message}<br>`, '');
       message = `The following errors occurred:<br>${message}`;
       showError(message);
       stopProgress(button, 'Signup');
@@ -148,9 +212,9 @@ const processLogin = async (e) => {
     if (status === 200) {
       localStorage.setItem('accessToken', parsedResponse.token);
       window.location.replace('stories.html');
-    } else if (Array.isArray(parsedResponse.error.message)) {
-      const errorList = parsedResponse.error.message;
-      let message = errorList.reduce((errStr, errObj) => `${errStr}<br>* ${errObj.msg}`, '');
+    } else if (status === 400) {
+      const errorList = parsedResponse.error;
+      let message = errorList.reduce((errStr, errObj) => `${errStr}* ${errObj.message}<br>`, '');
       message = `The following errors occurred:<br>${message}`;
       showError(message);
       stopProgress(button, 'Login');
@@ -175,103 +239,98 @@ formFields.forEach((field) => {
 });
 
 // -------------------- Stories.html functions --------------------------
+const storyCard = document.querySelector('#js-story-card');
+const storyList = document.querySelector('.js-story-list');
 
+const getStoryCard = () => document.importNode(storyCard.content, true);
 
-
-const fetchAllEntries = async (e) => {
-  e.preventDefault();
-  const button = document.querySelector('.js-login-button');
-
-  startProgress(button);
-
-  // get data from form fields
-  const email = document.querySelector('.js-login-email').value;
-  const password = document.querySelector('.js-login-password').value;
-
+// start a non-blocking fetch to endpoint
+const loadStories = async (queryObj) => {
+  // form query string from queryObj
+  const queryString = jsonToQuery(queryObj);
+  // set request url
+  const requestUrl = `${baseUrl}/entries${queryString}`;
   // prepare request
-  const data = JSON.stringify({
-    email,
-    password,
-  });
+  const token = localStorage.getItem('accessToken');
   const request = {
-    method: 'POST',
-    body: data,
-    headers: { 'Content-Type': 'application/json' },
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
   };
-
   // send request
   try {
-    const rawResponse = await fetch(`${baseUrl}/auth/login`, request);
+    // TODO: inform user that fetch has started
+    const rawResponse = await fetch(requestUrl, request);
     const { status } = rawResponse;
     const parsedResponse = await rawResponse.json();
     if (status === 200) {
-      localStorage.setItem('accessToken', parsedResponse.token);
-      window.location.replace('stories.html');
-    } else if (Array.isArray(parsedResponse.error.message)) {
-      const errorList = parsedResponse.error.message;
-      let message = errorList.reduce((errStr, errObj) => `${errStr}<br>* ${errObj.msg}`, '');
-      message = `The following errors occurred:<br>${message}`;
-      showError(message);
-      stopProgress(button, 'Login');
+      // when fetch is complete, iterate over entries
+      const { entries } = parsedResponse;
+      // foreach iteration, populate the story-card template with entry details
+      if (entries.length) {
+        entries.forEach((entry) => {
+          const {
+            id, title, content, created_on: createdOn,
+          } = entry;
+          const storyDate = new Date(createdOn);
+          const card = getStoryCard();
+          card.querySelector('.story-list__item').setAttribute('data-story-id', id);
+          card.querySelector('.date__day').innerHTML = storyDate.getDay();
+          card.querySelector('.date__month').innerHTML = storyDate.getMonth();
+          card.querySelector('.date__year').innerHTML = storyDate.getFullYear();
+          card.querySelector('.caption__title').innerHTML = title;
+          card.querySelector('.caption__content').innerHTML = `${content.substring(0, 100)}...`;
+          card.querySelector('.item__link').href = `story.html?id=${id}`;
+          // append node to story-list
+          storyList.appendChild(card);
+          // TODO: hide progress toast
+        });
+      } else {
+        const storyDate = new Date();
+        const card = getStoryCard();
+        card.querySelector('.date__day').innerHTML = storyDate.getDay();
+        card.querySelector('.date__month').innerHTML = storyDate.getMonth();
+        card.querySelector('.date__year').innerHTML = storyDate.getFullYear();
+        card.querySelector('.caption__title').innerHTML = 'You have not added any story';
+        card.querySelector('.caption__content').innerHTML = 'Click on this card to add your first story';
+        card.querySelector('.item__link').href = 'new-story.html';
+        card.querySelector('.item__overlay').innerHTML = 'Write a story';
+        // append node to story-list
+        storyList.appendChild(card);
+        // TODO: hide progress toast
+      }
     } else {
-      showError(parsedResponse.error.message);
-      stopProgress(button, 'Login');
+      handleCommonErrors(status, parsedResponse);
     }
   } catch (err) {
-    showError('An error occurred');
-    stopProgress(button, 'Login');
+    // TODO: toast connection error message
   }
 };
 
-// -------- Single Story functions --------------------------
 
-let editMode = false;
+// -------------------- Router functions --------------------------
 
-const modal = document.querySelector('.js-modal');
-const storyDeleteBtn = document.querySelector('.js-delete-story');
-const storyEditBtn = document.querySelector('.js-edit-story');
-const modalCancelBtn = document.querySelector('.js-cancel-modal');
-const modalConfirmBtn = document.querySelector('.js-confirm-modal');
-const favBtn = document.querySelector('.js-icon-heart');
-
-const showDeleteModal = () => {
-  modal.style.display = 'block';
-};
-const hideDeleteModal = () => {
-  modal.style.display = 'none';
-};
-
-const confirmDelete = () => {
-  // DELETE fetch operation
-  modal.style.display = 'none';
-};
-
-const toggleFavorite = (e) => {
-  e.target.classList.toggle('icon--heart-active');
-};
-
-const editStory = (e) => {
-  const el = document.querySelector('.js-story-content');
-  if (!editMode) {
-    const range = document.createRange();
-    const sel = window.getSelection();
-    range.setStart(el.childNodes[0], 0);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
-    el.focus();
-    e.target.innerHTML = 'Save';
-    editMode = true;
-  } else {
-    /* perform fetch operation */
-    e.target.innerHTML = 'Edit';
-    editMode = false;
-    window.focus();
+const resolveRoute = () => {
+  // get url
+  const path = window.location.pathname;
+  const queries = queryToJSON(window.location.search);
+  // match path -- switch(n)
+  switch (path) {
+    case '/stories.html':
+      loadStories(queries);
+      break;
+    case '/story.html':
+      loadStory(queries);
+      break;
+    case '/profile.html':
+      loadProfile();
+      break;
+    default:
   }
 };
 
-if (storyDeleteBtn) storyDeleteBtn.addEventListener('click', showDeleteModal);
-if (storyEditBtn) storyEditBtn.addEventListener('click', editStory);
-if (modalConfirmBtn) modalConfirmBtn.addEventListener('click', confirmDelete);
-if (modalCancelBtn) modalCancelBtn.addEventListener('click', hideDeleteModal);
-if (favBtn) favBtn.addEventListener('click', toggleFavorite);
+// on page load, resolve route
+window.addEventListener('DOMContentLoaded', resolveRoute);
